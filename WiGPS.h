1ac3ae324c2c43c157bd83a30587827b0b6d5db3
Geoffrey Card <gercunderscore4@gmail.com>
2014-06-15 23:04:45 -0700

    added float value for seconds

--- WiGPS.h
+++ WiGPS.h
@@ -1,18 +1,18 @@
 /*
  Copyright 2013 Daniele Faugiana
- 
+
  This file is part of "WiGPS Arduino Library".
- 
+
  "WiGPS Arduino Library" is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  "WiGPS Arduino Library" is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
- 
+
  You should have received a copy of the GNU General Public License
  along with "WiGPS Arduino Library". If not, see <http://www.gnu.org/licenses/>.
  */
@@ -28,6 +28,8 @@
  *                Fixed latitude and longitude seconds
  *                Added get functions
  *                Formatted to YYYY-MM-DD HH:mm:ss
+ *                Added seconds as float for more precision
+ *                Update loop exits on timeout
  */
 
 
@@ -41,12 +43,12 @@
  * Data constants
  *****************/
 
-#define UPDATE_TIMEOUT 5000; // attempt update for 5 seconds
-#define CONV_TO_SEC 0.6 // converts centi-minutes to seconds // 60/100
+#define DEFAULT_UPDATE_TIMEOUT 5 // attempt update for 5 seconds
+#define CONV_TO_SEC 0.0006 // converts sub-1 minutes to seconds 0.mmmmm -> ss
 #define KNOTS_TO_MS 0.514444 // knots to m/s
-#define DEGREE_CHAR 0xBA // unicode not ASCII
-#define PROTOCOL   "GPRMC"
-#define BUFFER_LENGTH_2   75 //Had to change the name because made conflict with some other SparkCore define named BUFFER_LENGTH
+#define DEGREE_CHAR 0xDF // Hitachi degree
+#define PROTOCOL "GPRMC"
+#define BUFFER_LENGTH_2 75 //Had to change the name because made conflict with some other SparkCore define named BUFFER_LENGTH
 
 /*******************
  * GPS POWER STATES
@@ -91,17 +93,19 @@ private:
     int latitudeDeg;            // Last Latitude data from the GPS
     int latitudeMin;
     int latitudeSec;
+    float latitudeSecf;         // Seconds as a float, for more precision
     char latitudeRef;
 
     int longitudeDeg;           // Last Longitude data from the GPS
     int longitudeMin;
     int longitudeSec;
+    float longitudeSecf;        // Seconds as a float, for more precision
     char longitudeRef;
 
     int Speed;                  // Last speed from the GPS (km/h)
     int Course;                 // Last course over ground (degrees from the north)
 
-    int dataReady;              // Data ready to be read
+    bool dataReady;             // Data ready to be read
 
     /******************
      * PRIVATE METHODS
@@ -121,6 +125,7 @@ public:
     int on(void);                   // Powers on the GPS module
     int off(void);                  // Turns off the GPS module and stop tracking data
     bool update(void);              // Starts fetching data from the GPS.
+    bool update(int timeout_sec);   // Starts fetching data from the GPS.
 
     String time(void);              // Returns an Arduino String object for the UTC time
     String date(void);              // Returns an Arduino String object for the UTC date
@@ -129,18 +134,20 @@ public:
     String speed(void);             // Returns an Arduino String object for the speed
     String course(void);            // Returns an Arduino String object for the course
 
-	bool isReady(void);              // Returns dataReady, use to determine whether to read
+    bool isReady(void);             // Returns dataReady, use to determine whether to read
 
-	float getLatitude(void);        // Returns a float for the latitude in degrees
+    float getLatitude(void);        // Returns a float for the latitude in degrees
     int getLatitudeDeg(void);       // Last Latitude data from the GPS
     int getLatitudeMin(void);
     int getLatitudeSec(void);
+    float getLatitudeSecf(void);
     char getLatitudeRef(void);
 
     float getLongitude(void);       // Returns a float for the latitude in degrees
     int getLongitudeDeg(void);      // Last Longitude data from the GPS
     int getLongitudeMin(void);
     int getLongitudeSec(void);
+    float getLongitudeSecf(void);
     char getLongitudeRef(void);
 
     ~WiGPS();
